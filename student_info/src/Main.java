c9f21eeee723a3069dd8b61eea438029710b8d34
海洋之心 <1450065803@qq.com>
2018-11-22 21:10:36 +0800

    加入了参考资料文件

--- student_info/src/Main.java
+++ student_info/src/Main.java
@@ -1,15 +1,40 @@
 import java.sql.*;
 
+import java.awt.Color;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         //获取连接
-        connect a = new connect();
-        Connection con = a.getConnection();
+//        connect a = new connect();
+//        Connection con = a.getConnection();
+//        addbach_test(con);
+//        select_test(con);
+//        con.close();
 
-        addbach_test(con);
-        select_test(con);
-        con.close();
+        JFrame f = new JFrame("LoL");
+        f.setSize(400, 300);
+        f.setLocation(580, 200);
+        f.setLayout(null);
+        JCheckBox bCheckBox = new JCheckBox("物理英雄");
+        //设置 为 默认被选中
+        bCheckBox.setSelected(true);
+        bCheckBox.setBounds(50, 50, 130, 30);
+        JCheckBox bCheckBox2 = new JCheckBox("魔法英雄");
+        bCheckBox2.setBounds(50, 100, 130, 30);
+        //判断 是否 被 选中
+        System.out.println(bCheckBox2.isSelected());
+
+        f.add(bCheckBox);
+        f.add(bCheckBox2);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setVisible(true);
+
+
+
 
     }
 
